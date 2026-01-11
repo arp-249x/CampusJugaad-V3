eb773853b8e917817d7d22b42ceb25fb24d5a238
Arpan <mclaren2492005@gmail.com>
2026-01-11 20:27:32 +0530

    Added logic for Otp.ts

---
+++ backend/src/models/Otp.ts
@@ -0,0 +1,10 @@
+import mongoose from 'mongoose';
+
+const OtpSchema = new mongoose.Schema({
+  email: { type: String, required: true },
+  otp: { type: String, required: true },
+  createdAt: { type: Date, default: Date.now, expires: 300 } // Auto-delete after 5 mins
+});
+
+
+export const Otp = mongoose.model('Otp', OtpSchema);
